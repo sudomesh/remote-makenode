bf27f63f10705ce9b767b229f9d6dc1c27c4c0de
Marc Juul <github@juul.io>
2016-09-06 03:37:43 -0700

    improved wordlist and checking

--- server/checkdupes.js
+++ server/checkdupes.js
@@ -2,9 +2,14 @@
 
 var fs = require('fs');
 
+console.log("Checking wordlist for problems");
+
 var lines = fs.readFileSync('wordlist').toString().split('\n');
 
-console.log("Checking wordlist for duplicate words");
+if(lines.length != 256) {
+    console.error("Wordlist must be 256 words long exactly!");
+    process.exit(1);
+}
 
 var dupes = false;
 var i, j, wordI, wordJ;
@@ -14,14 +19,16 @@ for(i=0; i < lines.length; i++) {
         wordI = lines[i].toLowerCase().replace(/\s+/, '');
         wordJ = lines[j].toLowerCase().replace(/\s+/, '');
         if(!wordI) {
-            console.log("EMPTY OR INVALID ENTRY ON LINE:", i);
+            console.error("EMPTY OR INVALID ENTRY ON LINE:", i);
+            process.exit(1);
         }
         if(!wordJ) {
-            console.log("EMPTY OR INVALID ENTRY ON LINE:", j);
+            console.error("EMPTY OR INVALID ENTRY ON LINE:", j);
+            process.exit(1);
         }
 
         if(wordI == wordJ) {
-            console.log("DUPLICATE FOUND ON LINE", i, "and", j, lines[i]);
+            console.error("DUPLICATE FOUND ON LINE", i, "and", j, lines[i]);
             dupes = true;
         }
     }
@@ -29,7 +36,8 @@ for(i=0; i < lines.length; i++) {
 
 if(!dupes) {
     console.log("No duplicates found.");
+} else {
+    console.log("Duplicates found!");
+    process.exit(1);
 }
-
-console.log("Total number of words:", lines.length);
 
